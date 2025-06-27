53202a3baf21fcda3009780c1bfaabf54d207935
codingguy927 <leemitchell1245@gmail.com>
2025-06-27 04:00:40 -0700

    Restore Unauthorized component

---
+++ components/Unauthorized.jsx
@@ -0,0 +1,8 @@
+export default function Unauthorized() {
+  return (
+    <div className="p-6">
+      <h2 className="text-xl font-semibold">Unauthorized</h2>
+      <p>Please sign in to access the dashboard.</p>
+    </div>
+  );
+}
